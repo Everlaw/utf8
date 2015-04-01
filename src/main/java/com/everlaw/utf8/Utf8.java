d9148776c1fb1148bc526e52904c5cfebb15be09
Brandon Mintern <mintern@everlaw.com>
2015-03-31 21:07:15 -0700

    Imports UTF-8 functionality from Everlaw codebase

---
+++ src/main/java/com/everlaw/utf8/Utf8.java
@@ -0,0 +1,72 @@
+package com.everlaw.utf8;
+
+/**
+ * A utility class for constants and operations related to UTF-8.
+ *
+ * @author Brandon Mintern
+ */
+public class Utf8 {
+
+    /**
+     * Converts the Unicode code point beginning at {@code str[index]} to a UTF-8 representation
+     * packed into an {@code int} that can be unpacked as follows:
+     * <pre>{@code
+     *  int utf8 = Utf8.toPackedInt(str, index);
+     *  byte[] unpacked = new byte[4];
+     *  int i = 0;
+     *  do {
+     *      unpacked[i++] = (byte) (utf8 & 0xFF);
+     *      utf8 >>>= 8;
+     *  } while (utf8 != 0);
+     * }</pre>
+     * <p>
+     * Note: if this method returns successfully and
+     * {@code Character.isHighSurrogate(str.charAt(index))}, then the next character (if there are
+     * any) begins at {@code index + 2}.
+     *
+     * @throws IndexOutOfBoundsException    if {@code index >= str.length()}
+     * @throws IllegalArgumentException     if {@code str[index]} (possibly combined with
+     *                                      {@code str[index + 1]} for surrogate pairs) is not a
+     *                                      valid Unicode code point
+     */
+    public static int toPackedInt(String str, int index) {
+        // http://en.wikipedia.org/wiki/UTF-8#Description was helpful in implementing this method.
+        char c = str.charAt(index);
+        int ch;
+        if (Character.isHighSurrogate(c)) {
+            if (index + 1 >= str.length()) {
+                throw new IllegalArgumentException("Invalid high surrogate character at " + index);
+            }
+            char low = str.charAt(index + 1);
+            if (! Character.isLowSurrogate(low)) {
+                throw new IllegalArgumentException("Invalid surrogate pair at " + index);
+            }
+            ch = Character.toCodePoint(c, low);
+        } else if (Character.isLowSurrogate(c)) {
+            throw new IllegalArgumentException("Invalid low surrogate character at " + index);
+        } else if (Character.isDefined(c)) {
+            ch = c;
+        } else {
+            throw new IllegalArgumentException("Undefined character at " + index);
+        }
+        if (ch < 0x80) {
+            return ch;
+        }
+        int bytes;
+        if (ch < 0x800) {
+            bytes = 2;
+        } else if (ch < 0x10000) {
+            bytes = 3;
+        } else {
+            bytes = 4;
+        }
+        int leadingMask = -1 << (8 - bytes) & 0xFF;
+        int utf8 = 0;
+        for (; bytes > 1; bytes--) {
+            utf8 |= (ch & 0b0011_1111) | 0b1000_0000;
+            utf8 <<= 8;
+            ch >>= 6;
+        }
+        return utf8 | ch | leadingMask;
+    }
+}
---
+++ src/main/java/com/everlaw/utf8/Utf8Iterator.java
@@ -0,0 +1,70 @@
+package com.everlaw.utf8;
+
+import java.util.NoSuchElementException;
+import java.util.PrimitiveIterator;
+
+/**
+ * Iterates over the UTF-8 bytes of a given String. Each call to {@link #nextInt} returns an int
+ * between 0 and 255, inclusive. Each call to {@link #nextByte} returns {@code (byte) nextInt()}.
+ *
+ * @author Brandon Mintern
+ */
+public class Utf8Iterator implements PrimitiveIterator.OfInt {
+
+    private final String str;
+    /**
+     * When nonzero, holds additional UTF-8 bytes representing the unicode code point that
+     * corresponds to 1-2 chars from str. Refer to {@link Utf8#toPackedInt(String, int)}.
+     */
+    private int utf8 = 0;
+    /**
+     * The current index into the next character in str.
+     */
+    private int pos = 0;
+
+    public Utf8Iterator(String s) {
+        str = s;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return utf8 != 0 || pos < str.length();
+    }
+
+    public byte nextByte() {
+        return (byte) nextInt();
+    }
+
+    @Override
+    public int nextInt() {
+        if (utf8 != 0) {
+            int b = utf8 & 0xFF;
+            utf8 >>>= 8;
+            return b;
+        }
+        if (pos < str.length()) {
+            utf8 = Utf8.toPackedInt(str, pos);
+            pos += Character.isHighSurrogate(str.charAt(pos)) ? 2 : 1;
+            if (utf8 == 0) {
+                // Must handle NUL explicitly.
+                return 0;
+            }
+            return nextInt();
+        }
+        throw new NoSuchElementException();
+    }
+
+    /**
+     * Returns the number of bytes necessary to encode the remainder of the string. This method
+     * should not be called repeatedly, as its runtime increases linearly with the length of the
+     * remaining string.
+     */
+    public int remainingLength() {
+        String rest = str.substring(pos);
+        int length = com.google.common.base.Utf8.encodedLength(rest);
+        for (int u = utf8; u != 0; u >>>= 8) {
+            length++;
+        }
+        return length;
+    }
+}
